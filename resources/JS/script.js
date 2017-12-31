33d3667551f64c816986fab3d1464bbbf9473572
conmeconte <32534202+conmeconte@users.noreply.github.com>
2017-12-31 02:02:43 -0800

    added animations and mobile nav button

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -26,4 +26,51 @@ function initializeApp() {
         $('html,body').animate({scrollTop: $('.section-features').offset().top}, 1000);
     });
     
+
+
+/* Animation on Scroll */   
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    })
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+/*Mobile Nav*/
+
+    $('.js--nav-icon').click(()=>{
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i')
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else{
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    })
 }
+
+
+
+
+
+
+
+
+
